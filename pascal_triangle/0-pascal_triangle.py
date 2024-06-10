d53c09781f2e4f8ec64687687555e270f70cc7f0
PMayala <p.mayala@alustudent.com>
2024-06-10 09:30:21 +0000

    A Python function that generates Pascal's Triangle up to a given number of rows

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
 """
-A Python function that generates Pascal's Triangle up to a given number of rows.
+Module for generating Pascal's Triangle.
 """
+
+
 def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal's Triangle of n rows.
+
+    Args:
+        n (int): The number of rows in the triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's Triangle.
+
+    Examples:
+        >>> pascal_triangle(5)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+        >>> pascal_triangle(1)
+        [[1]]
+        >>> pascal_triangle(0)
+        []
+        >>> pascal_triangle(10)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], \
+[1, 5, 10, 10, 5, 1], [1, 6, 15, 20, 15, 6, 1], [1, 7, 21, 35, 35, 21, 7, 1], \
+[1, 8, 28, 56, 70, 56, 28, 8, 1], [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]]
+        >>> pascal_triangle(100)
+        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], \
+[1, 5, 10, 10, 5, 1], [1, 6, 15, 20, 15, 6, 1], [1, 7, 21, 35, 35, 21, 7, 1], \
+[1, 8, 28, 56, 70, 56, 28, 8, 1], [1, 9, 36, 84, 126, 126, 84, 36, 9, 1], \
+... (output truncated for brevity) ...]
+    """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    
-    return triangle
 
-if __name__ == "__main__":
-    def print_triangle(triangle):
-        """
-        Print the triangle
-        """
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-    
-    print_triangle(pascal_triangle(5))
+    return triangle
